885fd7c5562ba941832a0ecec5e4654233008dd9
Sangram-Sutram <sangram.swain@sutramsol.in>
2025-01-22 15:23:28 +0530

    com operaters

---
+++ Intro/comOperator_004.rb
@@ -0,0 +1,23 @@
+num1 = 200;
+num2 = 100;
+num3 = 200;
+
+if num1 == num2
+    puts "num1 is equal to num2"; 
+elsif num1 > num2
+    puts "num1 is greater than num2"; 
+elsif num1 < num2
+    puts "num1 is less than num2";
+elsif num1 === num3
+    puts "num1 is equal to num3";
+else
+    puts "num1 is not equal to num2 & num3";
+end
+
+puts num1 === num3;
+puts num1 == num3;
+puts num1 != num3;
+puts num1 > num3;
+puts num1 < num3;
+puts num1 >= num3;
+puts num1 <= num3;
